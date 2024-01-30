d77e73e4d94033a3eac53e77ed018d72e258d638
Olga Khaidoukova <khaidoukova@inbox.ru>
2024-01-30 16:54:58 +0500

    some new code for optimizing keyword search in tg channels

--- main.py
+++ main.py
@@ -1,79 +1,67 @@
+import os
 import json
-from telethon.sync import TelegramClient
-from datetime import date, datetime
+from dotenv import load_dotenv
+from telethon import TelegramClient
 
-from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.tl.types import InputMessagesFilterEmpty
+from telethon.tl.types import Channel
 
-api_id = 28258595
-api_hash = '2d7104075fe614ced8bfed59c88f9623'
-username = '@khaidoukova'
-# TOKEN = '6750164468:AAEqKTRq-J2wGXaZgiBCaxTjd1rZN-gZBTM'
+load_dotenv('.env')
+
+api_id = os.getenv('TELEGRAM_API_ID')
+api_hash = os.getenv('TELEGRAM_API_HASH')
+username = os.getenv('TELEGRAM_USERNAME')
+
+
 client = TelegramClient(username, api_id, api_hash, system_version="4.16.30-vxCUSTOM")
 
 client.start()
 
 
-async def dump_all_messages(channel, words):
-    """Записывает json-файл с информацией о всех сообщениях канала/чата"""
-    offset_msg = 0  # номер записи, с которой начинается считывание
-    limit_msg = 100  # максимальное число записей, передаваемых за один раз
+async def search_messages(channel, keywords, offset_msg):
+    all_messages = []
+    offset_file = f'offset_{channel}.txt'
+    result_file = f'result_{channel}.json'
 
-    all_messages = []  # список всех сообщений
-    total_messages = 0
-    total_count_limit = 100  # общее количество сообщений, которой парсится
+    # Получение объекта канала по username
+    entity = await client.get_entity(channel)
+    # проходим циклом по ключевым словам
+    for word in keywords:
 
-    class DateTimeEncoder(json.JSONEncoder):
-        '''Класс для сериализации записи дат в JSON'''
+        async for message in client.iter_messages(entity, search=word, filter=InputMessagesFilterEmpty(),
+                                                  offset_id=offset_msg):
 
-        def default(self, o):
-            if isinstance(o, datetime):
-                return o.isoformat()
-            if isinstance(o, bytes):
-                return list(o)
-            return json.JSONEncoder.default(self, o)
+            data = {
+                'message': message.text,
+                'date': message.date.strftime('%Y-%m-%d %H:%M:%S'),
+                'user_id': message.from_id.user_id if getattr(message.from_id, 'user_id', None) else None
+            }
+            print(data)
+            all_messages.append(data)
+            offset_msg = message.id
 
-    while True:
-        history = await client(GetHistoryRequest(
-            peer=channel,
-            offset_id=offset_msg,
-            offset_date=None, add_offset=0,
-            limit=limit_msg, max_id=0, min_id=0,
-            hash=0))
-        # print(history)
-        if not history.messages:
-            break
-        messages = history.messages
-        for message in messages:
-            if message.message is not None:
-                for word in words:
-                    if word in message.message:
+    with open(result_file, 'w', encoding='utf-8') as outfile:
+        json.dump(all_messages, outfile, ensure_ascii=False)
 
-                        try:
-                            data = {
-                                    'message': message.message,
-                                    'date': datetime.fromtimestamp(message.date).strftime('%Y-%m-%d %H:%M:%S'),
-                                    'user_id': message.from_id.user_id
-                            }
-                            all_messages.append(data)
-
-                        except:
-                            continue
-
-        offset_msg = messages[len(messages) - 1].id
-        total_messages = len(all_messages)
-        if total_count_limit != 0 and total_messages >= total_count_limit:
-            break
-
-        with open('new_messages.json', 'w', encoding='utf-8') as f:
-            json.dump(all_messages, f, ensure_ascii=False, cls=DateTimeEncoder)
+    with open(offset_file, 'w') as file:
+        file.write(str(offset_msg))
 
 
 async def main():
-    # url = input() # Ссылка на канал или чат
-    channel = await client.get_entity('https://t.me/zakazyfreelance')
-    words = ['разработка', 'python']
+    channel = '@zakazyfreelance'
+    keywords = ['python', 'дизайн']
+    offset_file = f'offset_{channel}.txt'
 
-    await dump_all_messages(channel, words)
+    offset_msg = 0
+
+    try:
+        with open(offset_file, 'r') as file:
+            offset_msg = int(file.read().strip())
+    except FileNotFoundError:
+        pass
+
+    await search_messages(channel, keywords, offset_msg)
+    await client.disconnect()
 
 
 with client:
